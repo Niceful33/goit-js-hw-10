50ed3e67e998042debf79a50bcdd6b4a4a69e3c8
Nadia Kalynjuk <103382849+Niceful33@users.noreply.github.com>
2023-01-15 17:02:46 +0100

    add files2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './css/styles.css';
-
+import debounce from 'lodash.debounce';
+import { callFetch } from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
+
+const inputData = document.querySelector('#search-box');
+console.log(inputData);
+let inputName = '';
+
+inputData.addEventListener('input', debounce(countriesName, DEBOUNCE_DELAY));
+
+function countriesName(evt) {
+  // console.log(evt.currentTarget.value);
+  inputName = evt.target.value.trim();
+  callFetch();
+}
+
+export { inputName };
+// let countriesOut = {};
